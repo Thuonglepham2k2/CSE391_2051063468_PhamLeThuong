2c067cd5cf692fabea41bca95e81dcb7bdf861a8
Thaonguyen0310 <2051060722@e.tlu.edu.vn>
2022-04-14 19:01:35 +0700

    update trang chu

--- js/login.js
+++ js/login.js
@@ -7,18 +7,18 @@ var users = [
     {
         username: "thao01@tlu.edu.vn",
         password: "123",
-        permission: "users"
+        permission: "students"
     },
     {
         username: "thuong02@tlu.edu.vn",
         password: "456",
-        permission: "users"
+        permission: "students"
         
     },
     {
         username: "trang03@tlu.edu.vn",
         password: "789",
-        permission: "users"
+        permission: "students"
     },
     ]
 
@@ -29,14 +29,10 @@ function Login() {
 
     var val = check_user(username, password, users);
     if (val != false) {
-        var date = new Date();
-        exp_date = date.getTime() + 10 * 24 * 60 * 60 * 1000;
-        date.setTime(exp_date);
-        document.cookie = "username=" + val.username + ";expires=" + date;
-        document.cookie = "permission=" + val.permission + ";expires=" + date;
+       
         if (val.permission == "admin") {
             location.href = "admin/index.html";
-        } else if (val.permission == "users") {
+        } else if (val.permission == "students") {
             location.href = "index.html";
         }
     } else {
